1801e222537ee0a49a03d0b14c3e85c2ea0b7aa6
giuseppesciacca <sciaccagi@gmail.com>
2023-05-08 16:01:08 +0200

    [add] file json with tasks + vardump this tasks

--- index.php
+++ index.php
@@ -1,1 +1,47 @@
 <?php
+
+$tasks_string = file_get_contents('tasks.json');
+$tasks_list = json_decode($tasks_string, true);
+var_dump($tasks_list);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo List</title>
+    <!-- Bootstrap CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+
+<body>
+    <header>
+        <h1>Todo List</h1>
+    </header>
+    <main>
+        <form action="" method="get">
+            <div class="mb-3">
+                <label for="task" class="form-label">Name</label>
+                <input type="text" class="form-control" name="task" id="" aria-describedby="helpId" placeholder="Inserisci task">
+            </div>
+        </form>
+
+        <ul>
+            <li></li>
+        </ul>
+    </main>
+
+
+    <!-- Bootstrap JS Bundle -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <!-- AXIOS CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <!-- Vue.js CDN -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+</body>
+
+</html>
