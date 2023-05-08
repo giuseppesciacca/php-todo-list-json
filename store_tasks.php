a766ee7eb44386c2ecd543f4ada3cd922aa9888d
giuseppesciacca <sciaccagi@gmail.com>
2023-05-08 20:13:33 +0200

    [add] task

--- store_tasks.php
+++ store_tasks.php
@@ -8,9 +8,6 @@ if (isset($_POST['new_task'])) {
     $tasks_string = file_get_contents('./tasks.json');
 
     $task_list = json_decode($tasks_string, true);
-    //var_dump($task_list);
-
-
 
     array_push($task_list, $new_task);
     var_dump($task_list);
