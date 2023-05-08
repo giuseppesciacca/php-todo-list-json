77135c1a8ce2019172cd2dbdd83d7ecef21d979d
giuseppesciacca <sciaccagi@gmail.com>
2023-05-08 17:59:51 +0200

    [change] array_pop with array_push

--- store_tasks.php
+++ store_tasks.php
@@ -7,7 +7,7 @@ var_dump($new_task);
 $tasks_string = file_get_contents('./tasks.json');
 
 $task_list = json_decode($tasks_string, true);
-var_dump($task_list);
+//var_dump($task_list);
 
-array_pop($task_list, $new_task);
+array_push($task_list, $new_task);
 var_dump($task_list);
