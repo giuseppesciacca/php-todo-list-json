903b32332a0ac550de7ee5f975d03a72ac89ca25
giuseppesciacca <sciaccagi@gmail.com>
2023-05-08 17:48:55 +0200

    [add] json decode in store_tasks

--- store_tasks.php
+++ store_tasks.php
@@ -1,5 +1,13 @@
 <?php
-
 $new_task = $_POST['new_task'];
+//$new_task = 'prova';
 
 var_dump($new_task);
+
+$tasks_string = file_get_contents('./tasks.json');
+
+$task_list = json_decode($tasks_string, true);
+var_dump($task_list);
+
+array_pop($task_list, $new_task);
+var_dump($task_list);
